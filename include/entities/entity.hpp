6f2b76b9c932474c46864362e0ec1b30387068f2
Doglas <doglas_rocha@protonmail.ch>
2025-01-30 16:21:29 -0300

    feat: começar entidades

---
+++ include/entities/entity.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "../ente.hpp"
+#include <SFML/Graphics.hpp>
+#include "../managers/graphic_manager.hpp"
+
+using managers::GraphicManager;
+
+namespace entities
+{
+    class Entity : public Ente
+    {
+    protected:
+        int thrust = 0, x, y, velX, velY;
+
+    public:
+        Entity();
+        virtual ~Entity();
+        virtual void run();
+        virtual void setupEventHandling(
+            NOP::SharedAttribute<sf::Keyboard::Key> atKeyPressed,
+            NOP::SharedAttribute<sf::Keyboard::Key> atKeyReleased,
+            NOP::SharedAttribute<sf::Mouse::Button> atMouseButtonPressed,
+            NOP::SharedAttribute<sf::Mouse::Button> atMouseButtonReleased,
+            NOP::SharedAttribute<int> atMousePositionX,
+            NOP::SharedAttribute<int> atMousePositionY) = 0;
+        virtual void draw() = 0;
+        int getThrust() const;
+        virtual void setThrust(int thrust) = 0;
+        virtual sf::Drawable *getDraw() = 0;
+        int getX() const;
+        int getY() const;
+        void setX(const int x);
+        void setY(const int y);
+        void setVelX(const int vel);
+        void setVelY(const int vel);
+        int getVelX() const;
+        int getVelY() const;
+    };
+}
