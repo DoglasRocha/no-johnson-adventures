5d38d0b6387c71c8e289f089a63d02a180d02f8e
Doglas <doglas_rocha@protonmail.ch>
2025-03-04 12:06:48 -0300

    fix: namespace

--- include/entities/entity.hpp
+++ include/entities/entity.hpp
@@ -36,6 +36,6 @@ namespace entities
         void setVelY(const int vel);
         int getVelX() const;
         int getVelY() const;
-        virtual FloatRect getGlobalBounds() = 0;
+        virtual sf::FloatRect getGlobalBounds() = 0;
     };
 }
